677fa7c4a20cf0ba15b789135e47d41c34955188
ks_nh.92 <0046isk@gmail.com>
2021-06-29 10:03:53 +0700

    final update

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-def appendAndDelete(s, t, k):
-    # Write your code here
-    index=0
-    if len(s)>len(t):
-        for i in range(0,len(t)):
-            if(s[i]==t[i]):
-                index+=1
-        if (2*(len(t)-index)+len(s)-len(t))<=k:
-            return 'Yes'
-        else:
-            return 'No'
-    else:
-        for i in range(0,len(s)):
-            if(s[i]==t[i]):
-                index+=1
-                
-        print(index)
-        if (len(t)-index+len(t)-index)<=k:
-            return 'Yes'
-        else:
-            return 'No'
-
-print(appendAndDelete(input(),input(),int(input())))
+# passing and Arbitrary NUm
+import header
+def doSth(*arr):
+    for i in arr:
+        print(i)
+    return arr
+doSth(3,5,7,9)
+print(doSth(3,5,9))
+#passing an Arbitrary Number first step is initial tuple and after then
+#We push list argument to commit tupple
+def dicArbitrary(dau,duoi,**dic):
+    dic['dau']=dau
+    dic['duoi']=duoi
+    return dic
+print(dicArbitrary('cr7','cc',nhan='dep trai',nhu='xau vc'))
+dictionary={'nhan':1,'nhu':5}
+dictionary['cr7']=0
+print(dictionary)
+muldic={
+    'nhan':{
+        'HP':5,'Dame':10,
+        'skill':'chuong'
+    },
+    'nhu':{
+        'HP':1, 'Dame':20,
+        'skill':'dam'
+    }
+}
+for key in muldic.keys():
+    for key0 in muldic[key].keys():
+        print(f"{key}:{key0}:{muldic[key][key0]}")
+from header import doSth as ds
+print(ds(15))
